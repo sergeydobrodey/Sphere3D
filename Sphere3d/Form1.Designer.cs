3dfc4c909ab5be2678eda462bf122dfc373993b8
sergeydobrodey <sergey.dobrodey@hotmail.com>
2012-09-11 10:20:03 +0300

    revert to old repo

---
+++ Sphere3d/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Sphere3d
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rX = new System.Windows.Forms.RadioButton();
+            this.rY = new System.Windows.Forms.RadioButton();
+            this.rZ = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(544, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 148);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sphere";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(545, 201);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Радиус:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(597, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "40";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(569, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Rotate 45";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rZ);
+            this.panel1.Controls.Add(this.rY);
+            this.panel1.Controls.Add(this.rX);
+            this.panel1.Location = new System.Drawing.Point(569, 309);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 6;
+            // 
+            // rX
+            // 
+            this.rX.AutoSize = true;
+            this.rX.Checked = true;
+            this.rX.Location = new System.Drawing.Point(28, 14);
+            this.rX.Name = "rX";
+            this.rX.Size = new System.Drawing.Size(32, 17);
+            this.rX.TabIndex = 0;
+            this.rX.TabStop = true;
+            this.rX.Text = "X";
+            this.rX.UseVisualStyleBackColor = true;
+            // 
+            // rY
+            // 
+            this.rY.AutoSize = true;
+            this.rY.Location = new System.Drawing.Point(28, 37);
+            this.rY.Name = "rY";
+            this.rY.Size = new System.Drawing.Size(32, 17);
+            this.rY.TabIndex = 1;
+            this.rY.TabStop = true;
+            this.rY.Text = "Y";
+            this.rY.UseVisualStyleBackColor = true;
+            // 
+            // rZ
+            // 
+            this.rZ.AutoSize = true;
+            this.rZ.Location = new System.Drawing.Point(28, 61);
+            this.rZ.Name = "rZ";
+            this.rZ.Size = new System.Drawing.Size(32, 17);
+            this.rZ.TabIndex = 2;
+            this.rZ.Text = "Z";
+            this.rZ.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 623);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Location = new System.Drawing.Point(20, 20);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rZ;
+        private System.Windows.Forms.RadioButton rY;
+        private System.Windows.Forms.RadioButton rX;
+    }
+}
+
---
+++ Sphere3d/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace Sphere3d
+{
+    public partial class Form1 : Form
+    {
+        //int ox = 250;
+        //int oy = 250;
+
+        public class Point3d
+        {
+            public
+             int x, y, z;
+
+            public Point3d(double x, double y, double z)
+            {
+
+                this.x = (int)x;
+                this.y = (int)y;
+                this.z = (int)z;
+            }
+        }
+
+
+
+
+
+        public double[,] rotate(double angle)
+        {
+             double C = Math.Cos(angle);
+             double S = Math.Sin(angle);             
+             double[,] rotateX = { { 1, 0, 0 }, { 0, C, -S }, { 0, S, C } };
+             double[,] rotateY = { { C, 0, S }, { 0, 1, 0 }, { -S, 0, C } };
+             double[,] rotateZ = { { C, -S, 0 }, { S, C, 0 }, { 0, 0, 1 } };
+             if (rX.Checked) return rotateX;
+             if (rY.Checked) return rotateY;
+             if (rZ.Checked) return rotateZ;
+             else return null;
+            
+
+        }
+
+
+        public Point3d[] sphar = new Point3d[5000];
+
+        public void GetSphere(int R)
+        {
+            double x, y, z, fi, psi;
+            int i = 0;
+            for (psi = -Math.PI / 2; psi < Math.PI / 2; psi +=0.1)
+                for (fi = 0; fi < 2 * Math.PI; fi += 0.1)
+                {
+                    x = R * Math.Sin(psi) * Math.Cos(fi);
+                    y = R * Math.Sin(psi) * Math.Cos(fi);
+                    z = R * Math.Cos(psi);
+                    sphar[i] = new Point3d((int)x,(int)y,(int)z);
+                    i++;
+                }
+
+        }
+
+        
+
+
+        public void Draw2D(Graphics g, Point3d pt1, Point3d pt2)
+        {
+            Pen pen = new Pen(Color.Black, 1);
+
+            g.DrawLine(pen,new Point(pt1.x-pt1.z/2+250,-pt1.y+pt1.z/2+250),new Point(pt2.x-pt2.z/2+250,-pt2.y+pt2.z/2+250));
+        }
+
+
+        public Point3d Multiplicate(Point3d vertex, double[,] ar)
+        {
+           
+            vertex.x = Convert.ToInt32(vertex.x * ar[0, 0] + vertex.y * ar[1, 2] + vertex.z * ar[2, 0]);
+            vertex.y = Convert.ToInt32(vertex.x * ar[0, 1] + vertex.y * ar[1, 1] + vertex.z * ar[2, 1]);
+            vertex.z = Convert.ToInt32(vertex.x * ar[0, 2] + vertex.y * ar[1, 2] + vertex.z * ar[2, 2]);
+            return vertex;
+
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Graphics g = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Color.Red);
+            g.DrawLine(pen, new Point(250, 0), new Point(250, 250)); // y
+            g.DrawLine(pen, new Point(250, 250), new Point(500, 250)); //x
+            g.DrawLine(pen, new Point(72, 500-72), new Point(250, 250)); //z
+
+
+            double a = 2 * Convert.ToInt32(textBox1.Text) / Math.Sqrt(3);
+            int move = Convert.ToInt32(a);
+            
+            Draw2D(g, new Point3d(0, 0, 0), new Point3d(0, 0, move));
+            Draw2D(g, new Point3d(0, 0, move), new Point3d(move, 0, move));
+            Draw2D(g, new Point3d(move, 0, move), new Point3d(move, 0, 0));
+            Draw2D(g, new Point3d(move, 0, 0), new Point3d(0, 0, 0));
+
+            Draw2D(g, new Point3d(0, move, 0), new Point3d(0, move, move));
+            Draw2D(g, new Point3d(0, move, move), new Point3d(move, move, move));
+            Draw2D(g, new Point3d(move, move, move), new Point3d(move, move, 0));
+            Draw2D(g, new Point3d(move, move, 0), new Point3d(0, move, 0));
+
+
+            Draw2D(g, new Point3d(0, 0, 0), new Point3d(0, move, 0));
+            Draw2D(g, new Point3d(0, 0, move), new Point3d(0, move, move));
+            Draw2D(g, new Point3d(move, 0, move), new Point3d(move, move, move));
+            Draw2D(g, new Point3d(move, 0, 0), new Point3d(move, move, 0));
+      
+
+
+
+            //GetSphere(Convert.ToInt32(textBox1.Text));
+            //for (int i = 0; i < 2014; i++)
+            //    Draw2D(g, sphar[i], sphar[i + 1]);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double angle;
+            angle = Convert.ToDouble(textBox1.Text);           
+            Graphics g = pictureBox1.CreateGraphics();
+            Pen pen = new Pen(Color.Red);
+            g.DrawLine(pen, new Point(250, 0), new Point(250, 250)); // y
+            g.DrawLine(pen, new Point(250, 250), new Point(500, 250)); //x
+            g.DrawLine(pen, new Point(72, 500 - 72), new Point(250, 250)); //z
+
+
+            double a = 2 * Convert.ToInt32(textBox1.Text) / Math.Sqrt(3);
+            int move = Convert.ToInt32(a);
+            Point3d[] m;
+            m = new Point3d[8];
+            m[0] = new Point3d(0, 0, 0);
+            m[1] = new Point3d(0, 0, move);
+            m[2] = new Point3d(move, 0, move);
+            m[3] = new Point3d(move, 0, 0);
+            m[4] = new Point3d(0, move, 0);
+            m[5] = new Point3d(0, move, move);
+            m[6] = new Point3d(move, move, move);
+            m[7] = new Point3d(move, move, 0);
+            for (int i = 0; i < m.Length; i++)
+            {
+
+                m[i] = Multiplicate(m[i], rotate(angle));
+              
+            }
+
+        
+            for (int i = 0; i < 3; i++)
+                Draw2D(g, m[i], m[i + 1]);
+            Draw2D(g, m[3], m[0]);
+            for (int i = 4; i < 7; i++)
+                Draw2D(g, m[i], m[i + 1]);
+            Draw2D(g, m[4], m[7]);
+            Draw2D(g, m[0], m[4]);
+            Draw2D(g, m[1], m[5]);
+            Draw2D(g, m[2], m[6]);
+            Draw2D(g, m[3], m[7]);
+           
+        }
+    }
+}
+
