038365b0b1f75241dd17548e26a1e8f68e99c9a3
sergeydobrodey <sergey.dobrodey@hotmail.com>
2012-09-17 16:13:13 +0300

    Point3d class extracted

--- Sphere3d/Form1.cs
+++ Sphere3d/Form1.cs
@@ -9,19 +9,7 @@ namespace Sphere3d
     public partial class mainForm : Form
     {        
 
-        public class Point3d
-        {
-            public
-             float x, y, z;
-
-            public Point3d(double x, double y, double z)
-            {
-
-                this.x = (float)x;
-                this.y = (float)y;
-                this.z = (float)z;
-            }
-        }
+        
 
         #region global variables
         List<Point3d> sh = new List<Point3d>();  
---
+++ Sphere3d/Point3d.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sphere3d
+{
+    public class Point3d
+    {
+        public
+         float x, y, z;
+
+        public Point3d(double x, double y, double z)
+        {
+
+            this.x = (float)x;
+            this.y = (float)y;
+            this.z = (float)z;
+        }
+    }
+}
