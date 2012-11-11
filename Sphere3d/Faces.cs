cc3b6daced005357277bccb1df493ab5d65fd3f2
Sergey Dobrodey <sergey.dobrodey@hotmail.com>
2012-11-11 12:40:43 +0300

    night work

--- Sphere3d/Faces.cs
+++ Sphere3d/Faces.cs
@@ -11,14 +11,17 @@ namespace Sphere3d
         public Point3d pt3;
         public Point3d pt4;
         public Color FaceColor;
-
+        private float _A;
+        private float _B;
+        private float _C;
+        private float _D;
         public Faces(Point3d pt1, Point3d pt2, Point3d pt3, Point3d pt4, Color color)
         {
             this.pt1 = pt1;
             this.pt2 = pt2;
             this.pt3 = pt3;
             this.pt4 = pt4;
-            this.FaceColor = color;
+            FaceColor = color;
         }
 
      
@@ -26,7 +29,7 @@ namespace Sphere3d
         public void LightDiffuse()
         {
             var vectorLight = new Point3d(0,200,200);
-            var vectorPoint = new Point3d(pt1.x, pt1.y, pt1.z);
+            var vectorPoint = new Point3d((pt1.x + pt3.x) / 2, (pt1.y + pt3.y) / 2, (pt1.z+pt3.z)/2);
             double cosTetta = (vectorLight.x * vectorPoint.x + vectorLight.y * vectorPoint.y +
                               vectorLight.z * vectorPoint.z) / (
                                                                Math.Sqrt(Math.Pow(vectorLight.x, 2) +
@@ -46,6 +49,24 @@ namespace Sphere3d
            if (light) e.FillClosedCurve(new SolidBrush(FaceColor), new[] { pt1.ToPoitntFxy(), pt2.ToPoitntFxy(), pt3.ToPoitntFxy(), pt4.ToPoitntFxy(),pt1.ToPoitntFxy()}, FillMode.Alternate);
            else e.DrawLines(new Pen(FaceColor,1), new[] { pt1.ToPoitntFxy(), pt2.ToPoitntFxy(), pt3.ToPoitntFxy(), pt4.ToPoitntFxy(), pt1.ToPoitntFxy() });
           
+        }
+
+        public void RobertsSet(Point3d innerPoint)
+        {
+            _A = pt1.y * (pt2.z - pt4.z) + pt2.y * (pt4.z - pt1.z) + pt4.y * (pt1.z - pt2.z);
+            _B = pt1.z * (pt2.x - pt4.x) + pt2.z * (pt4.x - pt1.x) + pt4.z * (pt1.x - pt2.x);
+            _C = pt1.x * (pt2.y - pt4.y) + pt2.x * (pt4.y - pt1.y) + pt4.x * (pt1.y - pt2.y);
+            _D = -(_A * pt1.x + _B * pt1.y + _C * pt1.z);
+            if (innerPoint.x*_A + innerPoint.y*_B + innerPoint.z*_C + _D > 0) return;
+            _A = -_A;
+            _B = -_B;
+            _C = -_C;
+            _D = -_D;
+        }
+
+        public bool RobertsCheck()
+        {
+            return 0 * _A + 0 * _B + 10000 * _C + 1 * _D > 0;
         }
     }
 }
